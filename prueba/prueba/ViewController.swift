a160ef25eee4efd596fb3a60ca2ba1de43b66c62
guillermo <guillermouser.iti@hotmail.com>
2019-01-30 09:27:23 -0600

    memo

---
+++ prueba/prueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prueba
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 30/01/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
